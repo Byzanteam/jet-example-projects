fc167a686dba0932d6cda1ff53290d5d68da0dcd
Cold Cat <yuchengzhixia@gmail.com>
2024-10-24 11:14:23 +0800

    feat: tweak tower example project (#6)
    - [x] tweak tower example project to get directory users and groups

---
+++ tower_example/functions/main/db.ts
@@ -0,0 +1,41 @@
+import {
+  getEnvOrThrow,
+} from "https://cdn.jsdelivr.net/gh/byzanteam/breeze-js@v0.2.1/lib/runtime.ts";
+
+import { Kysely } from "kysely/index.js";
+import {
+  PostgresJSDialect,
+  setup,
+  wrapTransaction as wrapTransactionFn,
+} from "kysely-deno-postgres-dialect/mod.ts";
+import postgres from "postgresjs/mod.js";
+
+export interface UserTable {
+  name: string;
+}
+
+export interface Database {
+  users: UserTable;
+}
+
+setup(() => {
+  const dialect = new PostgresJSDialect({
+    postgres: postgres(
+      getEnvOrThrow("JET_DATABASE_URL"),
+      {
+        keep_alive: 600,
+        max: 10,
+      },
+    ),
+  });
+
+  return new Kysely<Database>({ // Database is defined by Kysely orm
+    dialect,
+  });
+});
+
+export async function wrapTransaction<T>(
+  callback: Parameters<typeof wrapTransactionFn<Database, T>>[0],
+): Promise<T> {
+  return await wrapTransactionFn<Database, T>(callback);
+}
